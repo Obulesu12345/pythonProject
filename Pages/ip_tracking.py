992bc6bf06937dfb3290c7bfaee4bde760aa7ed9
Obulesu12345 <Obulesu9390@gmail.com>
2024-04-05 16:43:03 +0530

    test

--- Pages/ip_tracking.py
+++ Pages/ip_tracking.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
+
 class Iptracking:
 
     def __init__(self,driver):
